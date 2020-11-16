b3d8f7ecf9a3ac3360beb9b95811427315835a2e
Amedee d'Aboville <amedee.daboville@gmail.com>
2020-11-15 19:33:06 -0500

    Cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
-use std::collections::BTreeSet;
-use std::fs::{read, read_to_string, File};
-use std::io::{self, prelude::*, BufReader};
-use std::str::from_utf8;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
-use fst::raw::Fst;
-use fst::{IntoStreamer, Set, SetBuilder, Streamer};
-use fst_gaddag::{build_entries, Gaddag};
+use fst_gaddag::Gaddag;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("dictionary.txt")?;
     let reader = BufReader::new(file);
     let set: Gaddag = Gaddag::from_words(reader.lines().map(|l| l.unwrap()));
-    /*
-    let words: BTreeSet<Vec<u8>> = build_entries(reader.lines().map(|l| l.unwrap()));
 
-    let wtr = io::BufWriter::new(File::create("dictionary.fst")?);
-    let mut build = SetBuilder::new(wtr).unwrap();
-    build.extend_iter(words).unwrap();
-    build.finish().unwrap();
-    println!("Done writing dictionary!");
-    let fst = Set::new(read("dictionary.fst")?)?;
-    println!("Done reading dictionary!");
-    let set: Gaddag = Gaddag::from_fst(fst);
-    */
     println!("dict contains COW : {} ", set.contains("COW"));
     println!("dict contains AA : {} ", set.contains("AA"));
     println!("dict words with .*TRING: {:#?} ", set.ends_with("TRING"));
